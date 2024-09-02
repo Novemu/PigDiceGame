20a0cd0f32bfb96d5c16dad9938daa94cdf19f20
Anthony <gonzalez.anthony0232@gmail.com>
2024-09-01 21:26:53 -0700

    Addded dice images and displayed and formatted the dice images on the page

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let firstPlayerScore = document.querySelector(".score--0");
+let secondPlayerScore = document.querySelector(".score--1");
+let firstPlayerCurrentScore = document.querySelector(".current--0");
+let secondPlayerCurrentScore = document.querySelector(".current--1");
+let newGame = document.querySelector(".btn--new");
+let rollDice = document.querySelector(".btn--roll");
+let hold = document.querySelector(".btn--hold");
